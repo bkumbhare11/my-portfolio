5674e9b928267f59a2af8f573a14ab779ae5b2b1
Bhavesh Kumbhare <bhaveshkumbhare18@gmail.com>
2026-03-14 17:27:39 +0530

    updated resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ function Navbar() {
         </ul>
       </nav>
       <a
-        href="https://drive.google.com/file/d/19PGC1PGHGkjZiVkyxNxYdL5WNOWRl4oi/view?usp=sharing"
+        href="https://drive.google.com/file/d/1ZIDBBrqB7t_RyRdt1whAlMK31VGuAsAc/view?usp=sharing"
         target="_blank"
         className="bg-black text-white px-3 py-1 rounded-lg transition-all duration-300 ease-in-out hover:bg-blue-600 hidden md:block"
       >
